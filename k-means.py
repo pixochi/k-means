bbdfa0a4803eb0a06a8214e257cdea94b6712a7a
pixochi <kozak.jakub55@gmail.com>
2019-04-11 01:14:23 +0200

    Add PCA

--- k-means.py
+++ k-means.py
@@ -28,16 +28,17 @@ dataset = dataset.rename(index=str, columns={"Samlet fravær": "average_absence"
 # Convert absence percentage to float
 absence_columns_selector = ['absence' in x for x in dataset.columns]
 absence_columns_indexes = dataset.columns[absence_columns_selector]
-absence_columns_indexes
 
 dataset[absence_columns_indexes] = (dataset[absence_columns_indexes]
     .replace('%', '', regex=True)
     .convert_objects(convert_numeric=True) / 100.0
 )
 
+# drop average absence so it's not used for mean, median, min, max, etc.
+absence_columns_indexes = absence_columns_indexes.drop(['average_absence'])
 absence_columns = dataset[absence_columns_indexes]
-absence_columns_indexes
-dataset['median_absence'] = absence_columns.drop(['average_absence'], 1).median(1)
+
+dataset['median_absence'] = absence_columns.median(1)
 
 # identify clusters of students in the dataset
 from sklearn.cluster import KMeans
@@ -73,3 +74,60 @@ plt.title("K-means")
 plt.xlabel("Median")
 plt.ylabel("Mean")
 plt.show()
+
+# use PCA
+from sklearn import decomposition
+from sklearn.preprocessing import Imputer
+
+imputer = Imputer(missing_values="NaN", strategy="mean", axis=0)
+
+numerical_absence_info = dataset[absence_columns_indexes]
+temp_numerical_absence_info = numerical_absence_info.copy()
+
+numerical_absence_info['average'] = dataset['average_absence']
+numerical_absence_info['median'] = temp_numerical_absence_info.median(1)
+numerical_absence_info['min'] = temp_numerical_absence_info.min()
+numerical_absence_info['max'] = temp_numerical_absence_info.max()
+
+# get rid of NaN values
+numerical_absence_info = imputer.fit_transform(temp_numerical_absence_info)
+
+plt.cla()
+pca = decomposition.PCA(n_components=2)
+pca.fit(numerical_absence_info)
+pcaFeatures = pca.transform(numerical_absence_info)
+
+plt.figure(figsize=(15, 12))
+plt.scatter(pcaFeatures[:, 0], pcaFeatures[:, 1], c=pcaFeatures[:, 1], edgecolor='none')
+plt.title("PCA")
+plt.xlabel("PC 1")
+plt.ylabel("PC 2")
+plt.show()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
